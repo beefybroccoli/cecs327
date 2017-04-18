2af58d4ec798dfa588433901e2e0803d9000cc04
calltedkim <tedkimemail@yahoo.com>
2017-04-18 09:54:19 -0700

    no message

---
+++ ServerProgrammingTed/src/main/java/stateful_server_client_version_4/StatefulServerListenerVersion4.java
@@ -0,0 +1,28 @@
+
+package stateful_server_client_version_4;
+
+import VALUE.VALUE;
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class StatefulServerListenerVersion4 {
+    
+        public static void main(String[] args) throws IOException {
+
+        boolean mListeningBoolean = true;
+
+        int id = 0;
+
+        System.out.println("Server Listener version 4 started");
+
+        try (ServerSocket serverSocket = new ServerSocket(VALUE.SERVER_PORT_NUMBER)) {
+
+            while (mListeningBoolean) {
+                new StatefulServerThreadVersion4(serverSocket.accept(), ++id).start();
+            }
+        } catch (IOException e) {
+            System.err.println("Could not listen on port " + VALUE.SERVER_PORT_NUMBER);
+        }
+    }
+
+}
